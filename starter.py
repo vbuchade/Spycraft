424d7119d776ad54d742755762fb3d7cbcf31837
Ling <30579642+linglin4@users.noreply.github.com>
2017-08-12 15:09:01 -0700

    Update starter.py

--- starter.py
+++ starter.py
@@ -3,10 +3,23 @@ from decryption import decrypt
 
 #input
 user_str=input("Please enter a string:")
+code_choice=input("Do you want to Encode or Decode?")
+while code_choice not in ("encode", "Encode", "Encrypt", "encrypt", "Decode", "Decode", "Decrypt", "decrypt"):
+    print("Please tell me if you want to Encode or Decode")
+    code_choice = input("Do you want to Encode or Decode?")
 
-#encode
-encoded_msg = encrypt(user_str)
-print("Encoded message = " + encoded_msg)
+if code_choice in ("encode", "Encode", "Encrypt", "encrypt"):
+    language_choice=input("Choose which language to encode to: English or Mandarin?")
+
+    while language_choice not in ("English", "english", "Mandarin", "mandarin"):
+        print("Please enter a supported language")
+        language_choice=input("Choose which language to encode to: English or Mandarin?")
+
+#encode to Mandarin
+if code_choice in ("encode", "Encode", "Encrypt", "encrypt"):
+    encoded_msg = encrypt(language_choice,user_str)
+    print("Encoded message = " + encoded_msg)
 
 #decode
-print("Decoded message = " + decrypt(encoded_msg))
+if code_choice in ("Decode", "decode", "Decrypt", "decrypt"):
+    print("Decoded message = " + decrypt(user_str))
