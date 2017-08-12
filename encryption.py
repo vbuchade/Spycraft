7295a4a6c4a7ba1079b74a09da942efef469b745
fuxiao224 <xiao.xfu24@gmail.com>
2017-08-12 15:04:36 -0700

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,9 +1,13 @@
-from dictionary_builder import encoder_dict as encoder
+from dictionary_builder import dictionary_builder
 
-def encrypt(user_str):
+def encrypt(lang, user_str):
+    encoder=dictionary_builder(lang)
     list1 = list(user_str)
-    print(list1)
     list2 = [[]] * len(list1)
     for i in range (0, len(list1)):
-        list2[i]=(encoder[list1[i]])
+        if list1[i] in dict.keys():
+            list2[i] = encoder[list1[i]]
+        else:
+            print("Error: Input keyword not found in Dictionary")
+
     return ''.join(list2)
